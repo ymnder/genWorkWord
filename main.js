8e1ed331f8f45ae28e76e66fe5deb3e27fafe8e1
ymnder <yamazaki.waseda@gmail.com>
2019-06-12 21:48:43 +0900

    fix broken string problem and rotate some marks

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const store = {
     outputImage: null,
     text: {
         content: "五千兆円欲しい",
-        output: "五千兆円欲しい",
+        output: '<span class="horizontal-content-text">五</span><span class="horizontal-content-text">千</span><span class="horizontal-content-text">兆</span><span class="horizontal-content-text">円</span><span class="horizontal-content-text">欲</span><span class="horizontal-content-text">し</span><span class="horizontal-content-text">い</span>',
         name: "IT・20代",
         outputName: "<span class='horizontal-content'>I</span><span class='horizontal-content'>T</span><span class='horizontal-content'>・</span><span class='horizontal-content'>2</span><span class='horizontal-content'>0</span><span class='horizontal-content'>代</span>",
         color: "#009051"
@@ -18,7 +18,10 @@ const methods = {
         }, 1000);
     },
     onInputContent: () => {
-        vm.text.output = vm.text.content.replace(/(?:\r\n|\r|\n)/g, '<br>');
+        vm.text.output = vm.text.content
+                                    .replace(/(.)/g, '<span class="horizontal-content-text">$1</span>')
+                                    .replace(/(〜|ー|、|。)/g, '<span class="rotate-text">$1</span>')
+                                    .replace(/(?:\r\n|\r|\n)/g, '<br>');
     },
     onInputName: () => {
         vm.text.outputName = vm.text.name
